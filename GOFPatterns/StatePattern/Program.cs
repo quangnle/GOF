3b95d788c924d5f71d9fd5a0ce413c1670ae9bf3
Cong Nguyen <shenlongbk@gmail.com>
2017-06-23 18:47:22 +0700

    add state pattern

---
+++ GOFPatterns/StatePattern/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var character = new Character();
+            for (int i = 0; i < 20; i++)
+            {
+                character.Update();
+                Thread.Sleep(500);
+            }
+        }
+    }
+
+    interface IState
+    {
+        void Execute(Character character);
+    }
+
+    class StandState : IState
+    {
+        public void Execute(Character character)
+        {
+            Console.WriteLine("Standing");
+            character.ChangeState(new WalkState());
+        }
+    }
+
+    class WalkState : IState
+    {
+        public void Execute(Character character)
+        {
+            Console.WriteLine("Walking");
+
+            if(MeetStone())
+                character.ChangeState(new JumpState());
+            else
+                character.ChangeState(new WalkState());
+        }
+
+        private bool MeetStone()
+        {
+            return (new Random()).NextDouble() < 0.3;
+        }
+    }
+
+    class JumpState : IState
+    {
+        public void Execute(Character character)
+        {
+            Console.WriteLine("Jumping");
+            character.ChangeState(new LandState());
+        }
+    }
+
+    class LandState : IState
+    {
+        public void Execute(Character character)
+        {
+            Console.WriteLine("Landing");
+            character.ChangeState(new WalkState());
+        }
+    }
+
+    class Character
+    {
+        private IState _state;
+
+        public Character()
+        {
+            _state = new StandState();
+        }
+
+        public void Update()
+        {
+            _state.Execute(this);
+        }
+
+        public void ChangeState(IState state)
+        {
+            _state = state;
+        }
+    }
+}
