79f9816af5d36e10d38b0984acd2685f78245087
Cong Nguyen <shenlongbk@gmail.com>
2017-06-26 12:34:08 +0700

    update memento code

--- GOFPatterns/MementoPattern/Program.cs
+++ GOFPatterns/MementoPattern/Program.cs
@@ -9,50 +9,45 @@ namespace MementoPattern
 {
     class Program
     {
-        private static readonly string MEMENTO_FILE_PATH = "memento.txt";
-
         static void Main(string[] args)
         {
             var robot = new Robot();
-
-            Memento savedMemento = new Memento(0, 0);
-            if(savedMemento.LoadFromFile(MEMENTO_FILE_PATH))
-            {
-                Console.WriteLine("Loaded old state from {0}", MEMENTO_FILE_PATH);
-                robot.SetMemento(savedMemento);
-            }
-
-            Console.WriteLine("(Initialized State) X = {0}, Y = {1}", robot.X, robot.Y);
+            var caretaker = new Caretaker();
 
             var command = "";
 
+            Console.WriteLine("Robot (X = {0}, Y = {1})", robot.X, robot.Y);
+
             while(command != "q")
             {
-                Console.Write("Enter command (u|d|l|r): ");
+                Console.Write("Enter command (up|down|left|right|undo): ");
                 command = Console.ReadLine().Trim();
 
                 switch(command)
                 {
-                    case "u": // go up
-                        robot.Y++;
+                    case "up": // go up
+                        caretaker.AddMemento(robot.CreateMemento());
+                        robot.Y++;                        
                         break;
-                    case "d": // go down
+                    case "down": // go down
+                        caretaker.AddMemento(robot.CreateMemento());
                         robot.Y--;
                         break;
-                    case "l": // go left
+                    case "left": // go left
+                        caretaker.AddMemento(robot.CreateMemento());
                         robot.X--;
                         break;
-                    case "r": // go right
+                    case "right": // go right
+                        caretaker.AddMemento(robot.CreateMemento());
                         robot.X++;
+                        break;
+                    case "undo":
+                        robot.SetMemento(caretaker.GetLastMemento());
                         break;
                 }
 
                 Console.WriteLine("X = {0}, Y = {1}", robot.X, robot.Y);
             }
-
-            var memento = robot.CreateMemento();
-            memento.SaveToFile(MEMENTO_FILE_PATH);
-            Console.WriteLine("Saved state to {0}", MEMENTO_FILE_PATH);
         }
     }
 
@@ -60,6 +55,29 @@ namespace MementoPattern
     {
         public int X { get; set; }
         public int Y { get; set; }
+    }
+
+    class Caretaker
+    {
+        private Stack<Memento> _mementoes;
+
+        public Caretaker()
+        {
+            _mementoes = new Stack<Memento>();
+        }
+
+        public void AddMemento(Memento m)
+        {
+            _mementoes.Push(m);
+        }
+
+        public Memento GetLastMemento()
+        {
+            if (_mementoes.Count == 0)
+                throw new Exception("no more memento");
+
+            return _mementoes.Pop();
+        }
     }
 
     class Memento
@@ -78,30 +96,6 @@ namespace MementoPattern
         public Position GetState()
         {
             return _pos;
-        }
-
-        public void SaveToFile(string path)
-        {
-            File.WriteAllText(path, string.Format("{0}\n{1}\n", _pos.X, _pos.Y));
-        }
-
-        public bool LoadFromFile(string path)
-        {
-            if (File.Exists(path))
-            {
-                var pos = File.ReadAllLines(path);
-                _pos = new Position()
-                {
-                    X = int.Parse(pos[0]),
-                    Y = int.Parse(pos[1])
-                };
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
     }
 
